e8714fc1c12edbc4a2c404d885e547bf554aee53
Art.Tseng <art.tseng@solartninc.com>
2020-09-15 13:57:48 +0800

    fix save entire array cookie bug

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -71,7 +71,7 @@ class LoginController extends Controller
         //     $token = config('line.access_token');
         // }
         $user_profile = $this->lineService->getUserProfile($token);
-        setcookie('user_profile', $user_profile, time() + 3600 * 24 * 28, '/');
+        //setcookie('user_profile', $user_profile, time() + 3600 * 24 * 28, '/');
         //echo "<pre>";
         //print_r($user_profile);
         //echo "</pre>";

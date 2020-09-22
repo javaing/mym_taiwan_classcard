f17990343d80076b6530c48f21c9d240f21d8079
Art.Tseng <art.tseng@solartninc.com>
2020-09-22 17:50:45 +0800

    for test get geo address

--- resources/views/classcard.blade.php
+++ resources/views/classcard.blade.php
@@ -9,6 +9,45 @@
         }}
 @endphp
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+    function getLocation() {
+        alert(position.coords);
+        if (navigator.geolocation) {
+            alert(position.coords.latitude);
+            navigator.geolocation.getCurrentPosition(savePosition, positionError, {
+                timeout: 10000
+            });
+        } else {
+            //Geolocation is not supported by this browser
+        }
+    }
+
+    // handle the error here
+    function positionError(error) {
+        var errorCode = error.code;
+        var message = error.message;
+
+        alert(message);
+    }
+
+    function savePosition(position) {
+        $.post("geo.php", {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        });
+    }
+</script>
+<button onclick="getLocation();">Get My Location</button>
+
+<div id="wrapper">
+    <?php
+    $ip = $_SERVER['REMOTE_ADDR'];
+    echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . $ip)));
+    ?>
+</div>
+
 <TABLE BORDER=0 CELLPADDING="4">
     <TR>
         <TD COLSPAN=4>{{ DBHelper::getUserName($card['UserID']) }} 你好</TD>

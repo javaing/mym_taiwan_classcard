2b3a8f6adf26c3dbfac9b6fa43e1230dbe7e9a2e
Art.Tseng <art.tseng@solartninc.com>
2020-10-21 09:50:07 +0800

    調整UI

--- resources/views/classhistory.blade.php
+++ resources/views/classhistory.blade.php
@@ -10,84 +10,90 @@
 }}
 @endphp
 
-<div align="right" style="margin-right: 30px;">
-    <p18>
-        {{ DBHelper::getUserName($card['UserID']) }} 你好
-    </p18>
-</div>
+<div style="height:100%;">
+    <TABLE BORDER=0 style="margin-bottom: 12px;width:100%;">
+        <TR>
+            <TD COLSPAN=2 align="right">
+                <p18>{{ DBHelper::getUserName($card['UserID']) }}</p18>
+            </TD>
+            <TD align="right">
+                <p18>您好</p18>
+            </TD>
+        </TR>
+    </TABLE>
 
-<div align="center">
-    <img style="margin-bottom: 12px;width:80%;" src="/images/div.png">
-</div>
-<div align="center" style="margin-bottom: 2px;">
+    <div align="center">
+        <img style="margin-bottom: 12px;width:80%;" src="/images/div.png">
+    </div>
+    <div align="center" style="margin-bottom: 2px;">
 
-    @if ($index-1>=0 )
+        @if ($index-1>=0 )
 
-    <a href="{{ route('show.classhistory', [
+        <a href="{{ route('show.classhistory', [
         'userId' => $card['UserID'], 
         'index'=>$index-1,
         ] ) }}">
+            <img style="width: 20px;height:20px" src="/images/arrow_left.png">
+        </a>
+        @else
         <img style="width: 20px;height:20px" src="/images/arrow_left.png">
-    </a>
-    @else
-    <img style="width: 20px;height:20px" src="/images/arrow_left.png">
-    @endif
+        @endif
 
-    <p16>上課紀錄</p16>
+        <p16>上課紀錄</p16>
 
-    @if ($index+1<$size ) <a href="{{ route('show.classhistory', [
+        @if ($index+1<$size ) <a href="{{ route('show.classhistory', [
         'userId' => $card['UserID'], 
         'index'=>$index+1,
         ] ) }}">
-        <img style="width: 20px;height:20px" src="/images/arrow_right.png">
-        </a>
-        @else
-        <img style="width: 20px;height:20px" src="/images/arrow_right.png">
-        @endif
-</div>
-<div align="center" style="margin-bottom: 24px">
-    <p16>{{ $index+1}}/{{ $size }}</p16>
-</div>
+            <img style="width: 20px;height:20px" src="/images/arrow_right.png">
+            </a>
+            @else
+            <img style="width: 20px;height:20px" src="/images/arrow_right.png">
+            @endif
+    </div>
+    <div align="center" style="margin-bottom: 24px">
+        <p16>{{ $index+1}}/{{ $size }}</p16>
+    </div>
 
-<TABLE BORDER=0 align="center">
-    <form>
-        @for ($i = 4; $i >= 1; $i--)
+    <TABLE BORDER=0 align="center">
+        <form>
+            @for ($i = 4; $i >= 1; $i--)
 
-        @if ($i%2==0 )
-        <TR>
+            @if ($i%2==0 )
+            <TR>
+                @else
+                @endif
+                <TD align="center">
+                    @if ($i> $card['Points'])
+                    <div id="div_used">
+                        @if ($consumeCount && sizeof($consumeCount)>(4-$i))
+                        <p14white>
+                            {{DBHelper::toDateString( $consumeCount[4-$i]['PointConsumeTime'] ) }}
+                        </p14white>
+                        @endif
+                    </div>
+                    @else
+                    <a href="{{ route('registe.classcard',  [$card['Points'], $card['CardID']]) }}">
+                        <div id="div_unuse"></div>
+                    </a>
+                    @endif
+                </TD>
+                @if ($i%2==1 )
+            </TR>
             @else
             @endif
-            <TD align="center">
-                @if ($i> $card['Points'])
-                <div id="div_used">
-                    @if ($consumeCount && sizeof($consumeCount)>(4-$i))
-                    <p14white>
-                        {{DBHelper::toDateString( $consumeCount[4-$i]['PointConsumeTime'] ) }}
-                    </p14white>
-                    @endif
-                </div>
-                @else
-                <a href="{{ route('registe.classcard',  [$card['Points'], $card['CardID']]) }}">
-                    <div id="div_unuse"></div>
-                </a>
-                @endif
-            </TD>
-            @if ($i%2==1 )
-        </TR>
-        @else
-        @endif
-        @endfor
-    </form>
-</TABLE>
+            @endfor
+        </form>
+    </TABLE>
 
-<div align="center" style="margin-Top: 8px;width:136px;height:36px;">
+    <div align="center" style="margin-Top: 8px;width:136px;height:36px;">
+    </div>
+    <div align="center" style="margin-Top: 24px;">
+        <a href="{{ route('reuse.line') }}">
+            <input type="image" style="width:136px;height:36px;" src="/images/classcard/back_classcard.png" alt="" />
+        </a>
+    </div>
+
 </div>
-<div align="center" style="margin-Top: 24px;">
-    <a href="{{ route('reuse.line') }}">
-        <input type="image" style="width:136px;height:36px;" src="/images/classcard/back_classcard.png" alt="" />
-    </a>
-</div>
-
-
 
 @endsection
--- resources/views/line.blade.php
+++ resources/views/line.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 <form>
     @if($url == 'reuse')
-    <a href={{ route('reuse.line') }}><img style="width: 100%;height:100%" src="/images/login_bg.png"></a>
+    <a href={{ route('reuse.line') }}><img style="height:100%" src="/images/login_bg.png"></a>
     @else
-    <a href="{{ $url }}"><img style="width: 100%;height:100%" src="/images/login_bg.png"></a>
+    <a href="{{ $url }}"><img style="height:100%" src="/images/login_bg.png"></a>
     @endif
 
 </form>

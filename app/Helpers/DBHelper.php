95bb83b7303b6f6e3fa2f9f6f867e1444297fa3d
Art.Tseng <art.tseng@solartninc.com>
2020-12-08 17:41:05 +0800

    無期限的卡也要orderby desc

--- app/Helpers/DBHelper.php
+++ app/Helpers/DBHelper.php
@@ -57,6 +57,7 @@ class DBHelper
             ->where('UserID', $userId)
             ->where('Expired', '=', null)
             ->where('Points', '>', 0)
+            ->orderBy('CardCreateTime', 'desc')
             ->first();
     }
 
@@ -76,6 +77,7 @@ class DBHelper
             ->where('UserID', $userId)
             ->where('Expired', '=', null)
             //->where('Points', '>', 0)
+            ->orderBy('CardCreateTime', 'desc')
             ->first();
     }
 

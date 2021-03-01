2b38fe32919d71e63726f23fb64696ce6eaa29b7
javaing63 <arttseng@gmail.com>
2021-03-01 23:22:06 +0800

    先mark排序作法，稍後再試

--- app/Helpers/DBHelper.php
+++ app/Helpers/DBHelper.php
@@ -317,9 +317,9 @@ class DBHelper
                             }
                         }
                     }
-        usort($totlaRecord, function ($item1, $item2) {
-          return $item1['Name'] <=> $item2['Name'];
-        });
+        // usort($totlaRecord, function ($item1, $item2) {
+        //   return $item1['Name'] <=> $item2['Name'];
+        // });
         return $totlaRecord;
     }
 

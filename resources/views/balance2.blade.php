b4aeb007b21e6ede20111ea9e8dd21315ccdcb21
javaing63 <arttseng@gmail.com>
2021-03-02 15:09:47 +0800

    try to find out git not work for balance2

--- resources/views/balance2.blade.php
+++ resources/views/balance2.blade.php
@@ -93,15 +93,13 @@ $range = $range ?? '';
 
 </div>
 
-<form action="download" method="POST">
+<form action="downloadByName" method="POST">
     @csrf
     <input type="hidden" name="filename" value="{{substr($start,0,10)}}_{{substr($end,0,10)}}">
     <input type="hidden" name="start" value="{{$start}}">
     <input type="hidden" name="end" value="{{$end}}">
     <button class="btn btn-sm btn-default" type="submit">下載報表</button>
 </form>
-
-<br>
 
 <form action="downloadByName" method="POST">
     @csrf

aab71548fbbcfb0f39f2beba8ae3e22e8f385c0e
javaing63 <arttseng@gmail.com>
2021-06-03 18:25:19 +0800

    TAIWAN都大寫

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <img style="margin-bottom:12px; width:80%;" src="/images/div.png">
     <br>
-    <p14>MYM Taiwan &copy; 2020</p14>
+    <p14>MYM TAIWAN &copy; 2020</p14>
 
 </div>

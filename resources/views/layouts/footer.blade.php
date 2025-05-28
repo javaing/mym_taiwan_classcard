989dd974f883c53932271c784b0b37afc244c8c2
javaing <arttseng@gmail.com>
2025-05-28 17:21:58 +0800

    Update footer.blade.php
    年份的字串自動抓取

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,9 @@
 <div class="container">
     <img style="margin-bottom:12px; width:80%;" src="/images/div.png">
     <br>
-    <p14>MYM TAIWAN &copy; 2020</p14>
+    <p14>MYM TAIWAN &copy; #current-year {
+    font-weight: bold;
+    color: white;
+}</p14>
 
 </div>
